792929d6691888c782f0465af67a6d7dcf00d9d4
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2025-05-28 00:14:49 +0200

    export vars

--- topk-rs/src/lib.rs
+++ topk-rs/src/lib.rs
@@ -10,4 +10,12 @@ pub use client::ClientConfig;
 pub use client::CollectionClient;
 pub use client::CollectionsClient;
 
+pub mod defaults {
+    pub use crate::client::RETRY_BACKOFF_BASE;
+    pub use crate::client::RETRY_BACKOFF_INIT;
+    pub use crate::client::RETRY_BACKOFF_MAX;
+    pub use crate::client::RETRY_MAX_RETRIES;
+    pub use crate::client::RETRY_TIMEOUT;
+}
+
 pub use client::retry;
