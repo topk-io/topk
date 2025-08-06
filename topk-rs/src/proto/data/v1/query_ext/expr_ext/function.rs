a703a03f89665d22578ffce192310b00b8b27e12
Marek Galovic <galovic.galovic@gmail.com>
2025-08-06 09:07:28 +0200

    .

--- topk-rs/src/proto/data/v1/query_ext/expr_ext/function.rs
+++ topk-rs/src/proto/data/v1/query_ext/expr_ext/function.rs
@@ -1,5 +1,5 @@
 use crate::proto::{
-    data::v1::{SparseVector, Vector},
+    data::v1::{list, value, SparseVector, Value, Vector},
     v1::data::{function_expr, FunctionExpr},
 };
 
@@ -69,5 +69,27 @@ impl From<Vec<u8>> for QueryVector {
     fn from(values: Vec<u8>) -> Self {
         #[allow(deprecated)]
         QueryVector::Dense(Vector::u8(values))
+    }
+}
+
+impl From<Value> for QueryVector {
+    fn from(value: Value) -> Self {
+        match value.value {
+            Some(value::Value::Vector(vector)) => QueryVector::Dense(vector),
+            Some(value::Value::SparseVector(sparse_vector)) => QueryVector::Sparse(sparse_vector),
+            Some(value::Value::List(list)) => match list.values {
+                #[allow(deprecated)]
+                Some(list::Values::F32(f32)) => QueryVector::Dense(Vector::f32(f32.values)),
+                #[allow(deprecated)]
+                Some(list::Values::U8(u8)) => QueryVector::Dense(Vector::u8(u8.values)),
+                Some(_) => {
+                    unimplemented!("Only list<f32> and list<u8> can be converted to QueryVector")
+                }
+                None => unimplemented!("List cannot be empty"),
+            },
+            _ => {
+                unimplemented!("Only Vector, SparseVector and List can be converted to QueryVector")
+            }
+        }
     }
 }
