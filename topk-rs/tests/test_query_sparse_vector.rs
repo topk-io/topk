bd4ab495b808347ed50d3cd880e7c6acae4a7fcd
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2025-08-07 09:17:20 +0200

    [test] dismbiguate sparse query ordering

--- topk-rs/tests/test_query_sparse_vector.rs
+++ topk-rs/tests/test_query_sparse_vector.rs
@@ -20,7 +20,10 @@ async fn test_query_sparse_vector_distance_f32(ctx: &mut ProjectTestContext) {
                     "sparse_f32_distance",
                     fns::vector_distance(
                         "sparse_f32_embedding",
-                        SparseVector::f32(vec![0, 1, 2], vec![1.0, 2.0, 3.0]),
+                        SparseVector::f32(
+                            vec![0, 1, 2, 3, 4, 5],
+                            vec![1.0, 2.0, 3.0, 1.0, 3.0, 2.0],
+                        ),
                     ),
                 )])
                 .topk(field("sparse_f32_distance"), 3, false),
@@ -30,7 +33,7 @@ async fn test_query_sparse_vector_distance_f32(ctx: &mut ProjectTestContext) {
         .await
         .expect("could not query");
 
-    assert_doc_ids_ordered!(result, ["mockingbird", "1984", "alchemist"]);
+    assert_doc_ids_ordered!(result, ["1984", "mockingbird", "pride"]);
 }
 
 #[test_context(ProjectTestContext)]
@@ -47,7 +50,7 @@ async fn test_query_sparse_vector_distance_u8(ctx: &mut ProjectTestContext) {
                     "sparse_u8_distance",
                     fns::vector_distance(
                         "sparse_u8_embedding",
-                        SparseVector::u8(vec![0, 1, 2], vec![1, 2, 3]),
+                        SparseVector::u8(vec![0, 1, 2, 3, 4, 5], vec![1, 2, 3, 1, 3, 2]),
                     ),
                 )])
                 .topk(field("sparse_u8_distance"), 3, false),
@@ -57,7 +60,7 @@ async fn test_query_sparse_vector_distance_u8(ctx: &mut ProjectTestContext) {
         .await
         .expect("could not query");
 
-    assert_doc_ids_ordered!(result, ["mockingbird", "1984", "alchemist"]);
+    assert_doc_ids_ordered!(result, ["1984", "mockingbird", "pride"]);
 }
 
 #[test_context(ProjectTestContext)]
