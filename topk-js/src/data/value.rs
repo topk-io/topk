d1269ddcdcf87ae8cf19c8919ea8af0e859aab62
Marek Galovic <galovic.galovic@gmail.com>
2026-01-02 17:25:09 +0100

    fix py/js build (#306)

--- topk-js/src/data/value.rs
+++ topk-js/src/data/value.rs
@@ -221,6 +221,9 @@ impl From<topk_rs::proto::v1::data::Value> for Value {
                     None => unreachable!("Invalid list proto"),
                 },
             }),
+            Some(topk_rs::proto::v1::data::value::Value::Matrix(..)) => {
+                todo!()
+            }
             Some(topk_rs::proto::v1::data::value::Value::Struct(..)) => {
                 todo!()
             }
