bef4a109d96077764c66a7b6eb6e7fa22523ed40
Peter Toth <peter@petertoth.me>
2026-02-24 14:56:02 +0100

    rm

--- topk-py/src/data/matrix.rs
+++ topk-py/src/data/matrix.rs
@@ -24,13 +24,6 @@ pub struct Matrix {
 
 impl Matrix {
     pub(crate) fn from_numpy_array(untyped: &Bound<'_, PyUntypedArray>) -> PyResult<Self> {
-        if true {
-            use numpy::{
-                dtype, PyArrayDescrMethods, PyArrayDyn, PyArrayMethods, PyUntypedArray,
-                PyUntypedArrayMethods,
-            };
-        }
-
         let shape = untyped.shape();
         let ndim = shape.len();
         let num_cols = match ndim {
