a4ece79e5658620912dbe6038f5fcf01078452ae
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2026-04-06 22:44:29 +0200

    [ctx] add support for images (#414)

--- topk-rs/tests/test_dataset_upsert.rs
+++ topk-rs/tests/test_dataset_upsert.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
+use futures_util::TryStreamExt;
+use rstest::rstest;
 use test_context::test_context;
+use test_context::AsyncTestContext;
 use topk_rs::proto::v1::ctx::file::InputFile;
 use topk_rs::proto::v1::data::Value;
 use topk_rs::Error;
@@ -8,7 +11,7 @@ use topk_rs::Error;
 mod utils;
 use utils::ProjectTestContext;
 
-use crate::utils::dataset::test_pdf;
+use crate::utils::dataset::{test_file, test_pdf};
 
 #[test_context(ProjectTestContext)]
 #[tokio::test]
@@ -102,4 +105,61 @@ async fn test_upsert_file_with_invalid_metadata(ctx: &mut ProjectTestContext) {
 
         assert!(matches!(handle, Err(Error::DocumentValidationError(_))));
     }
+}
+
+#[rstest]
+#[case("pdfko.pdf")]
+#[case("markdown.md")]
+#[case("pictures/sample.jpg")]
+#[case("pictures/sample.png")]
+#[case("pictures/sample.bmp")]
+#[case("pictures/sample.gif")]
+#[case("pictures/sample.tiff")]
+#[case("pictures/sample.webp")]
+#[tokio::test]
+#[ignore]
+async fn test_upsert_file(#[case] file: &str) {
+    let ctx = ProjectTestContext::setup().await;
+
+    // Create dataset
+    let dataset = ctx
+        .client
+        .datasets()
+        .create(ctx.wrap("test"))
+        .await
+        .expect("could not create dataset")
+        .into_inner()
+        .dataset
+        .unwrap();
+
+    // Upsert  file
+    let upsert = ctx
+        .client
+        .dataset(&dataset.name)
+        .upsert_file(file, test_file(file), HashMap::<String, Value>::new())
+        .await
+        .expect(&format!("could not upsert file: {file}"));
+
+    // Wait for handle to be processed
+    ctx.client
+        .dataset(&dataset.name)
+        .wait_for_handle(&upsert.handle, None)
+        .await
+        .expect("handle was not processed within timeout");
+
+    // Search and verify we get the file back
+    let results = ctx
+        .client
+        .search(file, [&dataset.name], 1, None, Vec::<String>::new())
+        .await
+        .expect("could not search")
+        .into_inner()
+        .try_collect::<Vec<_>>()
+        .await
+        .expect("could not collect search results");
+
+    assert_eq!(results.len(), 1);
+    assert_eq!(results[0].doc_id, file);
+
+    ctx.teardown().await;
 }
