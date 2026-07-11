9141bb7905d823aeb89896e365e95af5b1cf1411
Marek Galovic <galovic.galovic@gmail.com>
2026-07-11 10:58:54 -0700

    test invalid sort

--- topk-rs/tests/test_query_sort.rs
+++ topk-rs/tests/test_query_sort.rs
@@ -3,6 +3,7 @@ use test_context::test_context;
 use topk_rs::data::literal;
 use topk_rs::proto::v1::data::stage::sort_stage::SortOrder;
 use topk_rs::query::{field, select};
+use topk_rs::Error;
 
 mod utils;
 use utils::dataset;
@@ -77,4 +78,32 @@ async fn test_query_sort_by_multiple_with_literal(ctx: &mut ProjectTestContext) 
         .expect("could not query");
 
     assert_doc_ids_ordered!(result, ["pride", "moby", "gatsby", "hobbit"]);
+}
+
+#[test_context(ProjectTestContext)]
+#[tokio::test]
+async fn test_query_sort_by_too_many_fields(ctx: &mut ProjectTestContext) {
+    let collection = dataset::books::setup(ctx).await;
+
+    let err = ctx
+        .client
+        .collection(&collection.name)
+        .query(
+            select([("_id", field("_id"))])
+                .sort(
+                    (0..10)
+                        .map(|i| (field(format!("field_{i}")), SortOrder::Asc))
+                        .collect::<Vec<_>>(),
+                )
+                .limit(4),
+            None,
+            None,
+        )
+        .await
+        .expect_err("Query should have failed validation");
+
+    assert!(matches!(
+        err,
+        Error::InvalidArgument(s) if s.contains("Sort must have at most 8 expressions")
+    ));
 }
